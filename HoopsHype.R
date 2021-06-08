ea674c9e2feb6f98dc47a8bc418177f84bd73bd9
kieranireland <kierancireland@gmail.com>
2021-06-08 15:10:13 -0400

    Creating ggplot and fixing some bugs in code. Also, putting some filters on plot to make it look more aesthetic.

--- HoopsHype.R
+++ HoopsHype.R
@@ -1,5 +1,6 @@
 library("rvest")
 library("dplyr")
+library(tidyverse)
 hoops_hype_vec <- c("https://hoopshype.com/nba2k/2013-2014/", "https://hoopshype.com/nba2k/2014-2015/", "https://hoopshype.com/nba2k/2015-2016/", "https://hoopshype.com/nba2k/2016-2017/", "https://hoopshype.com/nba2k/2017-2018/", "https://hoopshype.com/nba2k/2018-2019/", "https://hoopshype.com/nba2k/2019-2020/", "https://hoopshype.com/nba2k/")
 bball_ref_vec <- c("https://www.basketball-reference.com/leagues/NBA_2014_advanced.html", "https://www.basketball-reference.com/leagues/NBA_2015_advanced.html", "https://www.basketball-reference.com/leagues/NBA_2016_advanced.html", "https://www.basketball-reference.com/leagues/NBA_2017_advanced.html", "https://www.basketball-reference.com/leagues/NBA_2018_advanced.html", "https://www.basketball-reference.com/leagues/NBA_2019_advanced.html", "https://www.basketball-reference.com/leagues/NBA_2020_advanced.html", "https://www.basketball-reference.com/leagues/NBA_2021_advanced.html")
 hoops_df <- data.frame()
@@ -21,19 +22,26 @@ for (year in 13:22){
     rvest::html_nodes("#div_advanced_stats")
   ref_table <- ref_table_class %>%
     rvest::html_table(header = TRUE) %>%
-    as.data.frame()
-  final_bball_table <- subset(ref_table, select = c(Player, G, PER, BPM))
-  bball_ref_df <- dplyr::bind_rows(bball_ref_df, final_table)
-  bball_df <- bball_ref_df[!duplicated(bball_ref_df$Player), ]
+    as.data.frame() %>%
+    dplyr::select(Player, G, MP, PER, BPM) %>%
+    dplyr::mutate(
+      G = as.numeric(G, na.rm = TRUE),
+      MP = as.numeric(MP, na.rm = TRUE),
+      PER = as.numeric(PER, na.rm = TRUE),
+      BPM = as.numeric(BPM, na.rm = TRUE)
+    )
+  bball_df <- ref_table[!duplicated(ref_table$Player), ]
   count <- count + 1
-  merged_yearly_df <- dplyr::inner_join(hoops_df, bball_df, by = 'Player') %>%
+  restricted_mins_df <- filter(bball_df, MP > 750)
+  merged_yearly_df <- dplyr::inner_join(hoops_df, restricted_mins_df, by = 'Player') %>%
     dplyr::mutate(Year = year)
   merged_total_df <- dplyr::bind_rows(merged_total_df, merged_yearly_df)
+  
 }
 
-plot(merged_total_df$Rating ~ merged_total_df$BPM)
-abline(lm(merged_total_df$Rating ~ merged_total_df$BPM))
-
-# How aligned are 2k ratings to real plus-minus/PER
-# Who's overrated/underrated?
-# Where does 2k hit or miss in terms of ratings?
+ggplot(data = merged_total_df, mapping = aes(x = Rating, y = BPM, label = Player)) +
+  geom_point() +
+  theme_bw() +
+  geom_smooth() + 
+  geom_label() +
+  facet_wrap(~ Year, nrow = 2)
